d3b3231579e9e76a6d8acb8b0b5e3aff7e2df146
Eirik Vullum <evullum@gmail.com>
2015-04-12 15:49:41 +0200

    use express for static and favicon

--- index.js
+++ index.js
@@ -6,102 +6,127 @@ const path = require('path');
 const url = require('url');
 const util = require('util');
 const marked = require('marked');
+const favicon = require('serve-favicon');
+const express = require('express');
+const app = express();
 
 const siteName = 'My Perfect Site'
 const blogPageName = 'blog';
+const pages = getContent('/pages');
+const articles = getContent('/articles');
+
+app.use(express.static('static'));
+
+app.use(favicon(__dirname + '/favicon.ico'));
+
+app.use(function(req, res) {
+  let route = req.url === '/' ?
+    path.parse('/home') :
+    path.parse(req.url);
+
+  let page = pages[route.base];
+  let article = articles[route.name];
+  let title = (page && page.title) || (article && article.title);
+  let isBlog = route.base === blogPageName;
+
+  res.write(renderHead(title) + '\n');
+  res.write(renderHeader(title) + '\n');
+
+  if (!page && !article) {
+    res.write(renderPage(pages['not-found']) + '\n');
+  } else {
+    if (page) res.write(renderPage(page));
+    if (isBlog) res.write(listArticles());
+    if (article) res.write(renderArticle(article));
+  }
+
+  res.write(renderFooter() + '\n');
+  res.write(renderEnd() + '\n');
+  res.end();
+});
+
+app.listen(3000);
 
 function getContent(dir) {
   return fs.readdirSync(__dirname + dir).reduce(function(result, entry) {
-    // split into meta section and content section
-    // create an object that contains
-    // - title
-    // - author
-    // - post time
-    // - etc..
-    // (depends on if it is an article or page)
-    result[path.basename(entry, '.txt')] = marked(fs.readFileSync(__dirname + dir + '/' + entry).toString());
+    let raw = fs.readFileSync(__dirname + dir + '/' + entry, 'utf-8').toString();
+    let regex = /(?:\r?\n){2}/g;
+    let meta = raw.split(regex)[0];
+    let content = raw.split(regex).slice(1).join('\n\n');
+
+    result[path.basename(entry, '.txt')] = {
+      title: meta.match(/^TITLE:(.*)$/m) && meta.match(/^TITLE:(.*)$/m)[1],
+      author: meta.match(/^AUTHOR:(.*)$/m) && meta.match(/^AUTHOR:(.*)$/m)[1],
+      date: meta.match(/^DATE:(.*)$/m) && meta.match(/^DATE:(.*)$/m)[1],
+      noMenu: meta.match(/^(NOMENU:1)$/m) ? true : false,
+      url: meta.match(/^URL:(.*)$/m) && meta.match(/^URL:(.*)$/m)[1],
+      content: marked(content)
+    };
+
     return result;
   }, {});
 }
 
-const pageContent = getContent('/pages');
-const articleContent = getContent('/articles');
-
-function head(page, article) {
+function renderHead(title) {
   return `
     <!DOCTYPE html>
     <html lang="en">
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>${siteName + ' ' + page + ' ' + article}</title>
-      <style>${fs.readFileSync(__dirname + '/style.css')}</style>
+      <title>${siteName + ' ' + title}</title>
+      <link rel="stylesheet" type="text/css" href="/style.css">
     </head>
     <body>
   `;
 }
 
-function end() {
+function renderEnd() {
   return `</body>`;
 }
 
-function header(page, article) {
+function renderHeader(title) {
   return `
-    <div>${siteName + ' ' + page + ' ' + article}</div>
-    <ul>${Object.keys(pageContent).map(function(page) { return '<li><a href="/' + page + '">' + page + '</a></li>'}).join('\n')}</ul>
+    <header>
+      <h1><a href="/">${siteName}</a></h1>
+      <ul>${
+        Object.keys(pages).map(function(page) {
+          return !pages[page].noMenu ? '<li><a href="/' + page + '">' + pages[page].title + '</a></li>' : null;
+        }).join('\n')
+      }</ul>
+    </header>
   `;
 }
 
 function renderPage(page) {
   return `
-    <div>${pageContent[page]}</div>
+    <main>${page.content}</main>
   `;
 }
 
 function renderArticle(article) {
   return `
-    <div>${articleContent[article]}</div>
+    <article>${article.content}</article>
   `;
 }
 
-function footer() {
+function renderFooter() {
   return `
-    <div>This is the footer</div>
+    <footer>
+      <div class="left"><a href="">© ${(new Date()).getFullYear() + ' ' + siteName}</a></div>
+      <div class="right">Powered by <a href="http://blip.me">Blip</a>.</div>
+    </footer>
   `;
 }
 
 function listArticles() {
   return `
-    <ul>${Object.keys(articleContent).map(function(article) { return '<li><a href="' + url.resolve('/articles/', article) + '">' + article + '</a></li>'}).join('\n')}</ul>
+    <section>
+      <ul>${
+        Object.keys(articles).map(function(article) {
+          return '<li><a href="' + url.resolve('/articles/', article) + '">' + articles[article].title + '</a></li>'
+        }).join('\n')
+      }</ul>
+    </section>
   `;
 }
-
-function notFound() {
-  return `Not found`;
-}
-
-http.createServer(function(req, res) {
-  let route = path.parse(req.url);
-  let page = pageContent[route.base];
-  let article = articleContent[route.name];
-  let isBlog = route.base === blogPageName;
-  let isHome = req.url === '/';
-
-  res.write(head(route.dir, route.name) + '\n');
-  res.write(header(route.dir, route.name) + '\n');
-
-  if (!page && !article && !isHome) {
-    res.write(notFound() + '\n');
-  } else {
-    if (page) res.write(renderPage(route.base));
-    if (isHome) res.write(renderPage('home'));
-    if (isBlog) res.write(listArticles());
-    if (article) res.write(renderArticle(route.name));
-  }
-
-  res.write(footer() + '\n');
-  res.write(end() + '\n');
-  res.write('req.url: ' + req.url + '\n');
-  res.write('route: ' + util.inspect(route));
-  res.end();
-}).listen(3000);
